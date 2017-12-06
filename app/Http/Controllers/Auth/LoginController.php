d5ea877d0e88d3c6cf7cbc92f733ac2a9b93c2dd
miguelrock22 <miguelrock222@gmail.com>
2017-12-06 07:36:00 -0500

    login redirect

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,4 +37,16 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Conditional redirect by role
+     */
+    protected function authenticated(Request $request, $user)
+    {
+        if ( $user->hasRole('Administrador') ) {// do your margic here
+            return redirect()->route('/admin/home');
+        }
+        return $this->redirectTo;
+    }
+    
 }
