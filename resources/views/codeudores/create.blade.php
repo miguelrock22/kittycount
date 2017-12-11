dc2795eb39a3d357436c7172991c87e78c8067d0
juan.cuartas <juan.cuartas@arkix.com>
2017-12-11 13:46:42 -0500

    formulario codeudores

--- resources/views/codeudores/create.blade.php
+++ resources/views/codeudores/create.blade.php
@@ -12,7 +12,7 @@
 
             <div class="box-body">
                 <div class="row">
-                    {!! Form::open(['route' => 'codeudores.store']) !!}
+                    {!! Form::open(['route' => 'codeudores.store', 'files'=>true]) !!}
 
                         @include('codeudores.fields')
 
